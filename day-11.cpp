5c730a1e84a541dfc23d4f8ace45c127607b9120
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-06-28 18:33:24 +0530

    Create day-11.cpp

---
+++ day-11.cpp
@@ -0,0 +1,29 @@
+//leetcode-128(longest consecutive)
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n= nums.size();
+        if(n==0) return 0;
+
+        sort(nums.begin(),nums.end());
+        int lastsmaller = INT_MIN;
+        int cnt=0;
+        int longest =1;
+
+        //finding the longest sequence
+        for (int i=0; i<n; i++){
+            if(nums[i]-1 == lastsmaller){
+                cnt+=1;
+                lastsmaller = nums[i];
+            }
+
+            else if(nums[i]!=lastsmaller){
+                cnt =1;
+                lastsmaller=nums[i];
+
+            }
+            longest = max(longest , cnt);
+        }
+        return longest;
+    }
+};
