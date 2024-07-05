bbc27d0e5d6e4f60c2792ca25dad7be16bdefa38
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-07-05 22:21:23 +0530

    Create day-18.cpp
    in question - i have to solve in logN but i have solved it n N .  i will also upload next time

---
+++ day-18.cpp
@@ -0,0 +1,17 @@
+//leetcode - 33(search in rotated sorted array)
+
+//T.C.- o(N)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n =nums.size();
+        for(int i =0; i<n; i++){
+            if (nums[i]==target) {
+                return i;
+            }
+            
+        }
+      return -1;
+    }
+};
