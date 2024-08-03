237609ced80e41005590ba82bce9a3a2b2ca23d4
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-08-03 23:28:23 +0530

    Create day47.cpp

---
+++ day47.cpp
@@ -0,0 +1,21 @@
+//leetcode = 58 - (length of last word)
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int count = 0;
+
+        
+        for (int i = n - 1; i >= 0; i--) {
+           
+            if (s[i] != ' ') {
+                count++;
+            } else if (count > 0) {
+              
+                break;
+            }
+        }
+
+        return count;
+    }
+};
