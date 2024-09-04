a674bdb806f5537d9496046e90629ed91cbca811
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-09-04 23:58:22 +0530

    Create day-79.cpp

---
+++ day-79.cpp
@@ -0,0 +1,30 @@
+//leetcode -543(diameter of binary tree)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   
+public:
+ int res =0;
+ int height(TreeNode* root){
+        
+        if(root==nullptr) return 0;
+        int lh =  height(root->left);
+        int rh =  height(root->right);
+        res= max(res,1+lh+rh);
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return res-1;
+       
+    }
+};
