534a389ab9588ab944be4e7c8a38fb5f9bd61ff9
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-09-09 01:32:18 +0530

    Update day-83.cpp

--- day-83.cpp
+++ day-83.cpp
@@ -1,1 +1,29 @@
+//leetcode -83(remove duplicates from sorted list)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == nullptr) return head;
 
+        ListNode* ptr = head;
+        while(ptr->next != nullptr){
+            if(ptr->val == ptr->next->val){
+                ptr->next = ptr->next->next;
+            }
+            else{
+                ptr= ptr->next;
+            }
+        }
+        return head;
+        
+    }
+};
