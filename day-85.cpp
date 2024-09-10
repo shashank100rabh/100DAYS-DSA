b94c9d9495545adb658da6e5d1724f0681f155d4
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-09-11 01:35:42 +0530

    Update day-85.cpp

--- day-85.cpp
+++ day-85.cpp
@@ -1,1 +1,46 @@
+//leetcode -54(spiral matrix)
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty()) return {};
 
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int top = 0, left = 0, bottom = m - 1, right = n - 1;
+
+        // Vector to store the final answer
+        vector<int> ans;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right across the top row
+            for (int i = left; i <= right; i++) {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            // Traverse from top to bottom along the right column
+            for (int i = top; i <= bottom; i++) {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            // Check if there is a bottom row to traverse from right to left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Check if there is a left column to traverse from bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
