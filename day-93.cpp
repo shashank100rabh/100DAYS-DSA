8047cdd6fd02ef47f00460e3e22a4f7d65a08267
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-09-20 01:11:53 +0530

    Update day-93.cpp

--- day-93.cpp
+++ day-93.cpp
@@ -1,1 +1,27 @@
+//corparate flight bookings
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> ans(n, 0);  
 
+        int start, end, seats;
+
+        for(int i = 0; i < bookings.size(); i++) {
+            start = bookings[i][0];
+            end = bookings[i][1];
+            seats = bookings[i][2];
+
+            ans[start - 1] += seats;  
+            if(end < n) {
+                ans[end] -= seats;  
+            }
+        }
+
+        
+        for(int i = 1; i < n; i++) {
+            ans[i] += ans[i - 1];
+        }
+
+        return ans;
+    }
+};
