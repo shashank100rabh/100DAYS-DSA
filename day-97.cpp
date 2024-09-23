3e1866ba9af3a51213d5872ead979f25dae2d2f8
Shashank_Saurabh <111383498+shashank100rabh@users.noreply.github.com>
2024-09-24 00:30:29 +0530

    Create day-97.cpp

---
+++ day-97.cpp
@@ -0,0 +1,12 @@
+//leetcode -172(factorial trailing zeroes)
+class Solution {
+public:
+    int trailingZeroes(int n) { int sum=0;
+        while(n>0){
+            n=n/5;
+            sum=sum+n;
+        }
+        return sum;
+        
+    }
+};
